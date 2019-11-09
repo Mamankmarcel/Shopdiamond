8533e40162521f6648e7f827a8addc6b54ed3a21
Mamankmarcel <57529908+Mamankmarcel@users.noreply.github.com>
2019-11-09 17:22:16 +0700

    Add files via upload

---
+++ Shopdiamond.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+clear
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+bln="\033[33m"
+bc="\033[0m"
+echo "$grey jasa jual diamond"
+sleep 2
+echo "beli di youtube Mamank marcel"
+sleep 1
+echo "$green karna, disini saya menjual diamond dengan harga rendah"
+echo "agar konsumen percaya bahwa mamank marcel menjual diamond asli"
+sleep 10
+echo "yakin untuk beli diamond MLBB? "
+echo "chat di WA 085877666990"
+sleep 10
+exit
